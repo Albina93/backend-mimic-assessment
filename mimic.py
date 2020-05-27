7d6fffa5dca18fac29114ad920bf8652bf9d3316
Albina1993 <alba.tleubergen@gmail.com>
2020-05-27 16:04:38 -0400

    cleaning up the code

--- mimic.py
+++ mimic.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-"""Mimic exercise
-
-Read in the file specified on the command line.
-Do a simple split() on whitespace to obtain all the words in the file.
-Rather than read the file line by line, it's easier to read it into
-one giant string and split it once.
-
-Note: the standard python module 'random' includes a random.choice(list)
-method which picks a random element from a non-empty list.
-
-You can try adding in line breaks around 70 columns so the output looks
-better.
-"""
 
 __author__ = "Albina Tileubergen-Thomas help from Daniel(instructor), Cheria Artis"
 
@@ -26,7 +6,6 @@ import sys
 
 
 def create_mimic_dict(filename):
-
     mimic_dict = {}
     f = open(filename, 'r')
     text = f.read()
@@ -43,13 +22,6 @@ def create_mimic_dict(filename):
 
 
 def print_mimic(mimic_dict, start_word):
-    """Given a previously created mimic_dict and start_word,
-    prints 200 random words from mimic_dict as follows:
-        - Print the start_word
-        - Look up the start_word in your mimic_dict and get its next-list
-        - Randomly select a new word from the next-list
-        - Repeat this process 200 times
-    """
     word = start_word
     for _ in range(200):
         print(word, end=' ')
